21bbca07bec416c9c635dc34aa684cf2d6fe7f93
sethRait <sethrait95@gmail.com>
2017-03-03 14:52:10 -0500

    flushing buffer

--- tcpproxy.c
+++ tcpproxy.c
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <poll.h>
 
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 4096
 
 int main(int argc, char *argv[]) {
 	ProxyParams proxy_params;
@@ -53,37 +53,52 @@ void HandleConnection(int client, int server) {
 	fds[0].events |= (POLLIN | POLLOUT);
 	fds[1].events |= (POLLIN | POLLOUT);
 	fds[1].fd = server;
-	int connection_status;
 	TransferData(fds, to_server, to_client);
 }
 
 void TransferData(struct pollfd *fds, char *to_server, char *to_client) {
 	int server_offset = 0;
 	int client_offset = 0;
+	int msg_sent = 0;
+	int msg_recieved = 0;
 	while(1) {
 		poll(fds, 2, -1);
 		// If there is data to read on the client socket and room in the buffer
-		if (fds[0].revents & POLLIN && server_offset < BUFFER_SIZE) {
+		if (fds[0].revents & POLLIN && server_offset < BUFFER_SIZE - 1) {
 			server_offset += read(fds[0].fd, to_server + server_offset, 
 						 	  	  BUFFER_SIZE - server_offset - 1);
 		}
 		// If the client can accept data and there is data to be sent
 		while (fds[0].revents & POLLOUT && client_offset > 0) {
 			client_offset -= write(fds[0].fd, to_client, client_offset);
+			if (client_offset == 0) {
+				memset(to_client, '\0', BUFFER_SIZE);
+				msg_recieved = 1;
+			}
 		}
 		// If there is data to read on the server socket and room in the buffer
-		if (fds[1].revents & POLLIN && client_offset < BUFFER_SIZE) {
+		if (fds[1].revents & POLLIN && client_offset < BUFFER_SIZE - 1) {
 			client_offset += read(fds[1].fd, to_client + client_offset,
 						  	  	  BUFFER_SIZE - client_offset - 1);
 		}
 		// If the server can accept data and there is data to be sent
 		while (fds[1].revents & POLLOUT && server_offset > 0) {
 			server_offset -= write(fds[1].fd, to_server, server_offset);
+			if (server_offset == 0) {
+				memset(to_server, '\0', BUFFER_SIZE);
+				msg_sent = 1;
+			}
 		}
 		if (server_offset < 0 || client_offset < 0) {
-			printf("Client disconnected\n");
+			fprintf(stderr, "Client disconnected\n");
 			return;
+		}
+		printf("to_server: %d\nto_client: %d\n", server_offset, client_offset);
+		usleep(250000);
+		if (server_offset == 0 && client_offset == 0 && msg_recieved == 1) {
+			char end[] = "\r\n\r\n";
+			write(fds[1].fd, end, 4);
+			msg_recieved = 0;
 		}
 	}
 }
-
